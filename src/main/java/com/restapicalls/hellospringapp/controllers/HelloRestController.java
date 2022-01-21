6478574315f5c062ebe1bc1d0cc1eb6b3bf38930
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-21 18:52:33 +0530

    UC-2:- Used GET Request method and passed name as query parameter.

--- src/main/java/com/restapicalls/hellospringapp/controllers/HelloRestController.java
+++ src/main/java/com/restapicalls/hellospringapp/controllers/HelloRestController.java
@@ -1,6 +1,8 @@
 package com.restapicalls.hellospringapp.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +19,15 @@ public class HelloRestController {
 	public String sayHello() {
 		return "Hello from BridgeLabz...!";
 	}
+
+	/*** UC-2:- Make REST Call to show Hello Mark from BridgeLabz . 
+	 * 
+	 * 	--> curl localhost:8080/hello/query?name=Mark -w "\n"
+	 * ***/
+	@RequestMapping(value = {"/query"} , method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + " from BridgeLabz.";
+	}
 }
+	
+
